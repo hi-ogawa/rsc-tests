c513d4b5c12a27d1d96a1e72cddaffa2626d707a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-12-16 15:34:08 +0900

    chore: update lazarv

--- lazarv/app/layout.jsx
+++ lazarv/app/layout.jsx
@@ -10,12 +10,18 @@ export default function Layout(props) {
       <body>
         <div style={{ display: "flex", gap: "0.5rem" }}>
           <Link to="/">[/]</Link>
+          <Link to="/client-package-in-client">[/client-package-in-client]</Link>
+          <Link to="/client-package-in-server">[/client-package-in-server]</Link>
+          <Link to="/client-in-server-package">[/client-in-server-package]</Link>
+        </div>
+        <div style={{ display: "flex", gap: "0.5rem" }}>
           <Link to="/cjs">[/cjs]</Link>
           <Link to="/cjs2">[/cjs2]</Link>
           <Link to="/context">[/context]</Link>
           <Link to="/context2">[/context2]</Link>
           <Link to="/context3">[/context3]</Link>
         </div>
+        <hr />
         {props.children}
       </body>
     </html>
